3dfa8ebae0d78d8382e1ead366273a7f98f6e59d
Guilherme Queiroz <68303458+guilhermee-dev@users.noreply.github.com>
2021-11-17 12:05:56 -0300

    hello api "/"

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,10 @@ app.use(express.json());
 
 app.use(router);
 
+app.get("/", (req, res)=>{
+    res.send("Hello API");
+});
+
 app.get("/github", (req, res) => {
     res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
 });
